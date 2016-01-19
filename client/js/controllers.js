c5446301f32fdacb3f9d0d8f6faac1cba4437cf0
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-19 12:14:45 -0800

    add login form and function to toggle forms

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,5 +1,20 @@
 app.controller('SLController', ['$scope', 
   function($scope){
-    $scope.test = "THIS IS A TEST";
+    $scope.view = {};
+    $scope.view.showSignupForm = true;
+    $scope.view.showLoginForm = false;
+
+    $scope.toggleForm = function(){
+      $scope.view.showSignupForm = !$scope.view.showSignupForm;
+      $scope.view.showLoginForm = !$scope.view.showLoginForm;
+    };
+
+    $scope.signup = function(newUser){
+      // need to write userService
+    };
+
+    $scope.login = function(user){
+      // need to write userService
+    };
   }
 ]);
