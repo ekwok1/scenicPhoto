f7346cd61325611b55369709c1d3758de799a7ee
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-18 21:52:49 -0800

    write initial server.js code

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+
+// app.use
+app.use("/css", express.static(path.join(__dirname, '../client/css')));
+app.use("/js", express.static(path.join(__dirname, '../client/js')));
+app.use("/templates", express.static(path.join(__dirname, '../client/js/templates')));
+
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// catch all
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+// serving localhost
+app.listen(3000, function(){
+  console.log("localhost3000 ready");
+});
