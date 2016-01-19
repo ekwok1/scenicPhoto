3df1525a8c96970e9e6aa89380528ff00cec8c48
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-18 23:57:19 -0800

    add boilerplate for index.html

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var routes = require("./routes/users.js");
 app.use("/css", express.static(path.join(__dirname, '../client/css')));
 app.use("/js", express.static(path.join(__dirname, '../client/js')));
 app.use("/templates", express.static(path.join(__dirname, '../client/js/templates')));
+app.use("/images", express.static(path.join(__dirname, '../client/images')));
 
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
