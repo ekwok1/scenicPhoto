f623f97c135d22c6276ee1a3c2c422179a502245
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-21 15:49:57 -0800

    separate concerns for services

---
+++ client/js/services/UserService.js
@@ -0,0 +1,27 @@
+app.service("userService", ['$http', '$window', '$location',
+  function($http, $window, $location){
+    return {
+      signup: function(newUser){
+        return $http.post("/api/users/signup", newUser);
+      },
+      login: function(user){
+        return $http.post("/api/users/login", user);
+      },
+      setCurrentUser: function(data){
+        $window.localStorage.setItem("token", data.data.token);
+        $window.localStorage.setItem("user", JSON.stringify(data.data.user));
+        console.log(data);
+      },
+      logout: function(){
+        $window.localStorage.clear();
+        $location.path("/home");
+      },
+      getCurrentUser: function(){
+        return JSON.parse($window.localStorage.getItem("user"));
+      },
+      getCurrentUserId: function(){
+        return JSON.parse($window.localStorage.getItem("user")).id;
+      }
+    };
+  }
+]);
