31a1bcb38185b04860ed23bc8741e52455481135
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-21 15:02:23 -0800

    separate concerns for controllers

--- client/js/controllers/PhotoController.js
+++ client/js/controllers/PhotoController.js
@@ -1,44 +1,3 @@
-app.controller('SLController', ['$scope', 'userService', '$location',
-  function($scope, userService, $location){
-    $scope.view = {};
-    $scope.view.showSignupForm = true;
-    $scope.view.showLoginForm = false;
-   
-    $scope.resetAlert = function(){
-      $scope.view.sErrors = null;
-      $scope.view.lErrors = null;
-    };
-
-    $scope.toggleForm = function(){
-      $scope.view.showSignupForm = !$scope.view.showSignupForm;
-      $scope.view.showLoginForm = !$scope.view.showLoginForm;
-      $scope.resetAlert();
-    };
-
-    $scope.signup = function(newUser){
-      userService.signup(newUser).then(function(data){
-        userService.setCurrentUser(data);
-        $location.path("/photos");
-        if (data.status===400){
-          $scope.view.sErrors = data.data;
-        }
-        $scope.newUser = {};
-      });
-    };
-
-    $scope.login = function(user){
-      userService.login(user).then(function(data){
-        userService.setCurrentUser(data);
-        $location.path("/photos");
-        if (data.status===400){
-          $scope.view.lErrors = data.data;
-        }
-        $scope.user = {};
-      });
-    };
-  }
-]);
-
 app.controller('PhotosController', 
   ['$scope', '$location', 'userService', 'currentUser', 'photos', 'photoService',
   function($scope, $location, userService, currentUser, photos, photoService){
@@ -163,6 +122,14 @@ app.controller("PhotoController",
     };
   }
 ]);
+
+
+
+
+
+
+
+
 
 
 
