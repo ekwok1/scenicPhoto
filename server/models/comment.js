4f4c339a3609285e0bc57b398cd1aa23eb4a9763
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-21 15:46:23 -0800

    add pre remove hook for comment schema

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var db = require("./index.js");
 
 var commentSchema = new mongoose.Schema({
   username: {
@@ -11,6 +12,19 @@ var commentSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Photo"
   }
+});
+
+commentSchema.pre('remove', function(next){
+  var comment = this;
+  var commentId = comment._id;
+  var photoId = comment.photo;
+  db.Photo.findOne({ '_id': photoId }, function(err, photo){
+    if (!photo) return next();
+    var index = photo.comments.indexOf(commentId);
+    photo.comments.splice(index, 1);
+    photo.save();
+    next();
+  });
 });
 
 var Comment = mongoose.model("Comment", commentSchema);
