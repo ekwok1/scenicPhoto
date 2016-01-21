88573d20263ac50a4423b5ee031874dad3b969ed
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-21 15:06:40 -0800

    create initial comment model

---
+++ server/models/comment.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var commentSchema = new mongoose.Schema({
+  username: {
+    type: String
+  },
+  comment: {
+    type: String,
+  },
+  photo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Photo"
+  }
+});
+
+var Comment = mongoose.model("Comment", commentSchema);
+
+module.export = Comment;
