b2e49d6eeecd2704ba0d98ca5c1b8b5262dd497b
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-22 13:03:13 -0800

    design new profile layout

--- client/js/controllers/ProfileController.js
+++ client/js/controllers/ProfileController.js
@@ -4,6 +4,18 @@ app.controller("ProfileController", ['$scope', 'currentUser', 'user',
     // from resolves
     $scope.currentUser = currentUser;
     $scope.user = user;
+
+    // SPA booleans
+    $scope.view = {};
+    if (currentUser.username === user.username) {
+      $scope.view.showEditProfile = true;
+      $scope.view.showFollow = false;
+    } else {
+      $scope.view.showEditProfile = false;
+      $scope.view.showFollow = true;
+    }
+
+    
     
   }
 ]);
