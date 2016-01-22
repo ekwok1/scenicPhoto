c6519f4cf8399528e99f3e69cde36ecbce99562f
Eugene Kwok <eugenekwok11@gmail.com>
2016-01-22 11:00:20 -0800

    debug for deploy #2

--- server/server.js
+++ server/server.js
@@ -25,4 +25,6 @@ app.get('*', function(req, res){
 });
 
 // serving localhost
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("localhost3000");
+});
